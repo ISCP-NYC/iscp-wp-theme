9b75059c97a2040547817887bc9a7a903ca435cd
Phil Lubliner <phillubliner@gmail.com>
2024-11-21 12:57:01 -0500

    update programs, fix title sizes

--- sections/events.php
+++ sections/events.php
@@ -6,27 +6,64 @@ $page_url = get_the_permalink();
 	<?php get_template_part('partials/nav') ?>
 	<?php get_template_part('partials/side') ?>
 	<div class="content">
-		<div class="wrapper upcoming">
-			<h2 class="title head">Current and Upcoming Events &amp; Exhibitions</h2>
+		<div class="wrapper upcoming module">
+			<h2 class="title head">Programs &amp; Exhibitions</h2>
+			<h3 class="title orange">Current and Upcoming</h3>
+		</div>	
 			<?php 
+			// $sorted_events = sort_upcoming_events();
+			// $count = sizeof( $sorted_events );
+			// if ( $count ):
+			// 	echo '<div class="events shelves grid items residents upcoming">';
+			// 	foreach( $sorted_events as $event ):
+			// 		$post = $event;
+			// 		global $post;
+			// 		get_template_part( 'sections/items/event' );
+			// 	endforeach;
+			// 	echo '</div>';
+			// else:
+			// 	get_template_part( 'partials/no-posts' );
+			// endif;
+			// wp_reset_query();
 			$sorted_events = sort_upcoming_events();
 			$count = sizeof( $sorted_events );
+			$count_class = 'cols_' . $count;
 			if ( $count ):
-				echo '<div class="events shelves grid items residents upcoming">';
+				echo '<div class="events module shelves grid upcoming ' . $count_class . '">';
+				echo '<div class="swiper image_slider">';
+				echo '<div class="left arrow swap">';
+				echo '<div class="icon default"></div>';
+				echo '<div class="icon hover"></div>';
+				echo '</div>';
+				echo '<div class="right arrow swap">';
+				echo '<div class="icon default"></div>';
+				echo '<div class="icon hover"></div>';
+				echo '</div>';
+				echo '<div class="swiper-wrapper eventsWrap">';
+				foreach( $sorted_events as $event ):
+					$et = get_field( 'event_type', $event->ID );
+					if( $et == 'open-studios' ):
+						array_unshift( $sorted_events, $event );
+					endif;
+				endforeach;
+				$sorted_events = array_slice( $sorted_events, 0, 3 );
 				foreach( $sorted_events as $event ):
 					$post = $event;
 					global $post;
-					get_template_part( 'sections/items/event' );
+					get_template_part( 'sections/items/home-event', null,
+						array(
+							'class' => 'swiper-slide'
+						)
+					);
 				endforeach;
 				echo '</div>';
-			else:
-				get_template_part( 'partials/no-posts' );
+				echo '</div>';
+				echo '</div>';
 			endif;
 			wp_reset_query();
 			?>
-		</div>
-		<div class="wrapper past">
-			<h2 class="title head">Past Events &amp; Exhibitions</h2>
+		<div class="wrapper past module">
+			<h3 class="title">Past Programs &amp; Exhibitions</h3>
 			<div class="filter">
 				<div class="bar">
 					<div class="select link dropdown type" data-filter="type" data-slug="<?php echo $slug ?>">
